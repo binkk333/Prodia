e8bd9f9e5e9dd83206b4cc136d1de82f846dae41
binkk333 <144783617+binkk333@users.noreply.github.com>
2024-01-17 09:23:50 +0000

    dmmmmmmmm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,30 +2,43 @@
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import axios from 'axios';
 const API_KEY = "c611dcb7-e485-4572-a208-591a6c09e28b"; // API KEY HERE
-    const sdxlURL = "https://api.prodia.com/v1/sdxl/generate"
+    const sdxlURL = "https://urchin-app-ynpbq.ondigitalocean.app/api/text2image"
 	let prompt = 'a usa girl'
 	let image_url=''
 	let costVnd = 0
+    let jobId=''
+    let verifyJobUrl=""
 	async function GetImage(){
 		image_url=''
 		costVnd = 0
-        const body={
-  "model": "dreamshaperXL10_alpha2.safetensors [c8afe2ef]",
-  "prompt": "a muscle mouse",
-  "style_preset": "3d-model",
-  "steps": 20,
+        const params={
+  "model": "sd_xl_base_1.0.safetensors [be9edd61]",
+  "prompt": prompt,
+  "style": "3d-model",
   "width": 1024,
   "height": 1024
 }
-		axios.post(sdxlURL,body, {
-  headers: {
-    'Accept':'application/json',
-    'content-type': 'application/json',
-    'X-Prodia-Key': API_KEY
-  }})
+		axios.get(sdxlURL, {
+            params: params})
 
   .then(function (response) {
-    console.log(response);
+    console.log(response.data);
+    image_url=response.data
+    // Remove "https://images.prodia.xyz/" and ".png"
+    jobId = image_url.replace(/^https:\/\/images\.prodia\.xyz\//, '').replace(/\.png$/, '');
+    verifyJobUrl = "https://api.prodia.com/v1/job/" + jobId
+console.log(verifyJobUrl);
+    axios.get(verifyJobUrl,{
+    headers: {
+        'X-Prodia-Key': API_KEY,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+.then((res) => console.log(res.data.status))
+.catch((err) => console.error(err));
+
+
   })
   .catch(function (error) {
     console.log(error);
@@ -43,6 +56,7 @@ const API_KEY = "c611dcb7-e485-4572-a208-591a6c09e28b"; // API KEY HERE
 <button on:click={GetImage}>Send to get Image</button>
 
 {#if image_url != ''}
-<img src="data:image/png;base64,{image_url}" alt="theheai" />
+<h1>{image_url}</h1>
+<img src={image_url} alt="theheai" />
 {/if}
 
